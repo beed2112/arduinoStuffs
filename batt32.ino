bd69dc598c9d7dba10632680d14a189b11c76713
beed2112 <32287825+beed2112@users.noreply.github.com>
2021-02-05 10:11:31 -0700

    Add files via upload
    Read battery for LilyGO TTGO T-Energy ESP32-WROVER - with 18650 Battery Holder

---
+++ batt32.ino
@@ -0,0 +1,21 @@
+#include <driver/adc.h>
+#include "esp_adc_cal.h"
+
+void setup() {
+Serial.begin(115200);
+esp_adc_cal_characteristics_t adc_chars;
+esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC_ATTEN_DB_2_5, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
+pinMode(14, OUTPUT);
+}
+
+void loop() {
+digitalWrite(14, HIGH);
+delay(1000);
+float measurement = (float) analogRead(35);
+float battery_voltage = (measurement / 4095.0) * 7.26;
+digitalWrite(14, LOW);
+Serial.print("battery_voltage  ");
+Serial.print(battery_voltage);
+Serial.println("");
+
+}
